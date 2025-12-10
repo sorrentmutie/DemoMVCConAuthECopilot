1ec04e99c7b4dbcae7634f00a8de28e1ab604f7d
salvatore sorrentino <salvatore.sorrentino@live.com>
2025-12-10 08:23:22 +0100

    Aggiungere i file di progetto.

---
+++ DemoMVCConAuthECopilot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoMVCConAuthECopilot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
